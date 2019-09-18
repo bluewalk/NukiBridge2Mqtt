237c137619eec1df66d9d74d3767d35b19b1ee22
bluewalk <noreply@bluewalk.net>
2019-09-18 08:23:43 +0200

    Changed enums

--- Net.Bluewalk.NukiBridge2Mqtt/Models/Enum/StateEnum.cs
+++ Net.Bluewalk.NukiBridge2Mqtt/Models/Enum/StateEnum.cs
@@ -2,19 +2,23 @@
 {
     /// <summary>
     /// State of the device
-    ///   [Smartlock]__[Opener]
     /// </summary>
     public enum StateEnum
     {
-        Uncalibrated__Untrained = 0,
-        Locked__Online = 1,
+        Uncalibrated = 0,
+        Untrained = 0,
+        Locked = 1,
+        Online = 1,
         Unlocking = 2,
-        Unlocked__RtoActive = 3,
+        Unlocked = 3,
+        RtoActive = 3,
         Locking = 4,
-        Unlatched__Open = 5,
+        Unlatched = 5,
+        Open = 5,
         UnlockedLockNGo = 6,
-        Unlatching__Opening = 7,
-        X__BootRun = 253,
+        Unlatching = 7,
+        Opening = 7,
+        BootRun = 253,
         MotorBlocked = 254,
         Undefined = 255,
     }
