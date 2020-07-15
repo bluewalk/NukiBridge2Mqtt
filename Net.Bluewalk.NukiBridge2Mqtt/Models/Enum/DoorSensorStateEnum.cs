6ec3ae226e4f488564a24e17f0d73eb283ab3995
bluewalk <noreply@bluewalk.net>
2020-07-15 09:49:24 +0200

    Added support for Door-sensor state

---
+++ Net.Bluewalk.NukiBridge2Mqtt/Models/Enum/DoorSensorStateEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace Net.Bluewalk.NukiBridge2Mqtt.Models.Enum
+{
+    /// <summary>
+    /// State of the door sensor
+    /// </summary>
+    public enum DoorSensorStateEnum
+    {
+        Deactivated= 1,
+        Closed = 2,
+        Opened = 3,
+        Unknown = 4,
+        Calibrating = 5
+    }
+}
