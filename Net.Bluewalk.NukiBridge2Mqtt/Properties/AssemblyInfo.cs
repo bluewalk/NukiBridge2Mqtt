a856027011d911e0bfe2ccfb7b9bb120dbb6458a
Délano <delano.reijnierse@zrefinery.nl>
2020-12-22 08:43:49 +0100

    Bumped version to 1.5.3.0

--- Net.Bluewalk.NukiBridge2Mqtt/Properties/AssemblyInfo.cs
+++ Net.Bluewalk.NukiBridge2Mqtt/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Net.Bluewalk.NukiBridge2Mqtt")]
+[assembly: AssemblyDescription("NukiBridge2Mqtt Core")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b98c6cf0-cdae-4cd9-b69a-83ee4d898c28")]
