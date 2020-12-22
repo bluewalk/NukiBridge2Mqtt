85ea09bed885225085cb0d40c66acfa4c46d3a4f
bluewalk <noreply@bluewalk.net>
2020-12-22 08:43:49 +0100

    Bumped version to 1.5.3.0

--- AssemblyVersionInfo.cs
+++ AssemblyVersionInfo.cs
@@ -4,7 +4,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("NukiBridge2Mqtt")]
 [assembly: AssemblyCompany("Bluewalk")]
 [assembly: AssemblyProduct("NukiBridge2Mqtt")]
 [assembly: AssemblyCopyright("Copyright © 2020 Bluewalk")]
@@ -26,4 +25,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.2.0")]
+[assembly: AssemblyVersion("1.5.3.0")]
