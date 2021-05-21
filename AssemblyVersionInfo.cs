c9d9e6cb0807c80b62de55d8f2c861308ce4d0a5
Délano <delano.reijnierse@zrefinery.nl>
2021-05-21 09:45:36 +0200

    Bumped version number

--- AssemblyVersionInfo.cs
+++ AssemblyVersionInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyCompany("Bluewalk")]
 [assembly: AssemblyProduct("NukiBridge2Mqtt")]
-[assembly: AssemblyCopyright("Copyright © 2020 Bluewalk")]
+[assembly: AssemblyCopyright("Copyright © 2021 Bluewalk")]
 [assembly: AssemblyTrademark("Bluewalk")]
 [assembly: AssemblyCulture("")]
 
@@ -24,5 +24,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.3.1")]
+[assembly: AssemblyVersion("1.5.3.2")]
